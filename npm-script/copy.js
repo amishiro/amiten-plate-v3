d9c0f274e882214d20405728fe588af0e7103e56
Ami <amishiro@amiten.co.jp>
2022-04-19 00:24:47 +0900

    fix: update

--- npm-script/copy.js
+++ npm-script/copy.js
@@ -1,11 +1,9 @@
-const copy = require('copy');
-const dotenv = require('dotenv');
-dotenv.config({ path: '.env.production' });
-
+const copy = require('copy')
+const dotenv = require('dotenv')
+dotenv.config({ path: `.env.${process.env.MODE}` })
 const baseDir = process.env.BASE_DIR ? process.env.BASE_DIR : ''
 
-copy('./public/**/*', `./dist/${baseDir}`, function(err, files) {
-    if (err) throw err;
-    // `files` is an array of the files that were copied
-  });
-
+copy('./public/**/*', `./dist/${baseDir}`, function (err, files) {
+  if (err) throw err
+  // `files` is an array of the files that were copied
+})
